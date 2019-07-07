f580b09308463113125f518b5a026c7426a87736
Mapgie Embedded <33968140+Magpie_Embedded@users.noreply.github.com>
2019-07-07 22:44:08 +0100

    Minor fixes, removed example test.

--- pysimpleapp/__init__.py
+++ pysimpleapp/__init__.py
@@ -4,6 +4,6 @@
 
 __author__ = """Tim Guite"""
 __email__ = "magpieembedded@gmail.com"
-__version__ = "__version__ = '0.1.1'"
+__version__ = "__version__ = '0.1.2'"
 
 from pysimpleapp.message import Message
