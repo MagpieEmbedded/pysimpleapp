7d15270395196f2b3fff5dbd6d81919bae7bf35e
Mapgie Embedded <33968140+Magpie_Embedded@users.noreply.github.com>
2019-07-10 19:36:31 +0100

    Stop Travis failing due to not being hooked up to PyPi

--- setup.py
+++ setup.py
@@ -40,6 +40,6 @@ setup(
     test_suite="tests",
     tests_require=test_requirements,
     url="https://github.com/MagpieEmbedded/pysimpleapp",
-    version="version='0.1.3'",
+    version="0.1.3",
     zip_safe=False,
 )
